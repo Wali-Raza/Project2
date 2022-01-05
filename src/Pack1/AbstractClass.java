5326a55e3eccc6d625c0fbbbd5baebcc8874129a
Wali Raza <walimanzoor321@gmail.com>
2022-01-05 14:38:39 +0500

    Example Code

--- src/Pack1/AbstractClass.java
+++ src/Pack1/AbstractClass.java
@@ -5,14 +5,17 @@ abstract class AbstractClass
     public abstract void m1();
     public abstract void m2();
 }
-class Sub1 extends AbstractClass
+abstract class Sub1 extends AbstractClass
 {
      public void m1()
      {
          System.out.println("M1 body");
      }
-     public void m2()
-     {
-         System.out.println("M2 body");
-     }
+}
+class Sub2 extends Sub1
+{
+    public void m2()
+    {
+        System.out.println("Sub2 M2");
+    }
 }
