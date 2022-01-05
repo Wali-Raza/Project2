40b80ceee7d3c08eed7bdec3d869edb533fd9e24
Wali Raza <walimanzoor321@gmail.com>
2022-01-05 14:42:39 +0500

    Example Code

---
+++ src/Pack1/Interf.java
@@ -0,0 +1,17 @@
+package Pack1;
+interface interff
+{
+    public void m1();
+    public void m2();
+}
+public class Interf implements interff
+{
+    public void m1()
+    {
+        System.out.println("M1");
+    }
+    public void m2()
+    {
+        System.out.println("M2");
+    }
+}
